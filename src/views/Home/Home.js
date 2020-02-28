b25a73c044802ea98e6ff7d14a6af337b572cb26
josefha <josef.karakoca@valtech.com>
2020-02-28 18:04:43 +0100

    Connection to Spotify Api

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,3 +1,48 @@
-import React from "react"
+import React, { useState, useEffect } from 'react';
+import * as SpotifyWebApi from 'spotify-web-api-js';
 
-export default () => <div>Hello start page!</div>
+export default () => {
+    document.title = "music-collab beta"
+    const token = process.env.GATSBY_SPOTIFY_TOKEN
+    let spotifyApi = new SpotifyWebApi()
+    spotifyApi.setAccessToken(token)
+
+
+    // spotifyApi.pause().then(function (data) {
+    //     console.log('User playlists', data);
+    // }, function (err) {
+    //     console.error(err);
+    // });
+
+    // spotifyApi.skipToNext().then(function (data) {
+    //     console.log('User playlists', data);
+    // }, function (err) {
+    //     console.error(err);
+    // });
+
+    // spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', function (err, data) {
+    //     if (err) console.error(err);
+    //     else console.log('Artist albums', data);
+    // });
+
+    // spotifyApi.getUserPlaylists('2020')
+    //     .then(function (data) {
+    //         console.log('User playlists', data);
+    //     }, function (err) {
+    //         console.error(err);
+    //     });
+
+    spotifyApi.getUserPlaylists()  // note that we don't pass a user id
+        .then(function (data) {
+            console.log('User playlists', data);
+        }, function (err) {
+            console.error(err);
+        });
+
+    useEffect(() => {
+        console.log("YES")
+
+    });
+
+    return (<div>Hello welcome to music-collab!</div>)
+} 
