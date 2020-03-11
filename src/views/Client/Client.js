02627651bd81c17d374621bd167599643baf5aba
Alex Wahlandt <alex.wahlandt@gmail.com>
2020-03-11 09:48:43 +0100

    implemented support to search for artists

--- src/views/Client/Client.js
+++ src/views/Client/Client.js
@@ -1,12 +1,14 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import * as SpotifyWebApi from 'spotify-web-api-js';
-import { Box, Button, Heading, TextInput, Paragraph } from 'grommet';
+import { Box, Button, Heading, TextInput, Paragraph, RadioButtonGroup } from 'grommet';
 import { Gremlin, Send } from 'grommet-icons';
 import { navigate } from 'gatsby';
 import AppBar from '../../common/components/AppBar'
 import AppWrapper from '../../common/components/AppWrapper'
 import { FirebaseContext } from "gatsby-plugin-firebase"
 import { GlobalStateContext } from '../../common/context/GlobalContextProvider'
+import sendSong1 from '../../common/api/spotify/sendSong'
+
 
 export default () => {
     const token = process.env.GATSBY_SPOTIFY_TOKEN
@@ -14,55 +16,92 @@ export default () => {
     spotifyApi.setAccessToken(token)
 
     const state = useContext(GlobalStateContext) || { roomId: "" };
-    const [name, setName] = useState("Alex");
 
-    const [searchQueary, setsearchQueary] = useState("");
-    const [searchResults, setSearchResults] = useState([]);
+
+    const [clientState, setClientState] = useState({name: "Alex",
+    searchCategory: "Song",
+    searchQuery: "",
+    searchResults:[],
+    topSongs:[],
+  })
 
     const firebase = useContext(FirebaseContext)
+
+    useEffect(() => {
+      if(clientState.topSongs.length > 0){
+        renderTopSongs(clientState.topSongs);
+      }
+
+    },[clientState.topSongs])
 
     var prev = null
     const onChange = event => {
         let queryTerm = event.target.value
-        setsearchQueary(queryTerm);
+        setClientState({...clientState, searchQuery:queryTerm});
         // abort previous request, if any
         if (prev !== null) {
             prev.abort();
         }
-        // store the current promise in case we need to abort it
-        prev = spotifyApi.searchTracks(queryTerm, { limit: 10 });
+
+        if(clientState.searchCategory === 'Song'){
+          prev = spotifyApi.searchTracks(queryTerm, { limit: 10 });
+        }else if(clientState.searchCategory === 'Artist'){
+          prev = spotifyApi.searchArtists(queryTerm, {limit:10});
+        }
+
         prev.then(function (data) {
             // clean the promise so it doesn't call abort
             prev = null;
             // ...render list of search results...
-            setSearchResults(data.tracks.items)
+            if(data && data.tracks && data.tracks.items){
+              setClientState({...clientState, searchResults: data.tracks.items});
+            }else{
+              setClientState({...clientState, searchResults: data.artists.items});
+            }
+
         }, function (err) {
             console.error(err);
         });
+
     }
 
+    const renderTopSongs = (topSongs) => {
+      console.log("renderTopSongs", topSongs);
+      return(    <Box style={{ margin: '10px' }} direction='column' flex>
+                {
+                    clientState.searchResults.map((song, key) =>
+                        <Box style={{ margin: '8px 0' }} direction='row' key={key}>
+                            <Button size="small" primary icon={<Send />} onClick={() => sendSong(song)} />
+                            <Paragraph style={{ paddingLeft: '10px' }}>{song.name}</Paragraph>
+                        </Box>)
+                }
+            </Box>)
+
+
+    }
+
+    const showTopSongs = (artist) => {
+      navigate('/artist', {state: {data: artist}});
+    }
 
     const sendSong = (song) => {
         let postData = {
-            sentBy: name,
+            sentBy: clientState.name,
             title: song.name,
             uri: song.uri,
             external_url: song.external_urls.spotify,
         }
 
-        let newSongKey = firebase.database().ref().child('room').push().key;
-        let updates = {};
-        updates['/room/' + state.roomId + '/' + newSongKey] = postData;
+        sendSong1(postData,state.roomId,firebase)
 
-        firebase.database().ref().update(updates);
     }
 
     const SongList = () => {
         return (
             <Box style={{ margin: '10px' }} direction='column' flex>
                 {
-                    searchResults.map((song) =>
-                        <Box style={{ margin: '8px 0' }} direction='row'>
+                    clientState.searchResults.map((song, key) =>
+                        <Box style={{ margin: '8px 0' }} direction='row' key={key}>
                             <Button size="small" primary icon={<Send />} onClick={() => sendSong(song)} />
                             <Paragraph style={{ paddingLeft: '10px' }}>{song.name}</Paragraph>
                         </Box>)
@@ -70,6 +109,29 @@ export default () => {
             </Box>
         )
     }
+
+    const ArtistList = () => {
+      return (
+            <Box style={{ margin: '10px' }} direction='column' flex>
+                {
+                    clientState.searchResults.map((artist, key) =>
+                        <Box style={{ margin: '8px 0' }} direction='row' key={key}>
+                            <Button size="small" primary icon={<Send />} onClick={() => showTopSongs(artist)} />
+                            <Paragraph style={{ paddingLeft: '10px' }}>{artist.name}</Paragraph>
+                        </Box>)
+                }
+            </Box>
+      );
+    }
+
+    const handleSearchCategory = (event) => {
+      if(event.target.value === 'Artist'){
+        setClientState({...clientState, searchCategory: "Artist"});
+      }else if(event.target.value ==='Song'){
+        setClientState({...clientState, searchCategory: "Song"});
+      }
+    }
+
 
 
     return (
@@ -80,10 +142,16 @@ export default () => {
             </AppBar>
             <Box style={{ margin: '20px' }} direction='column' flex overflow={{ horizontal: 'hidden' }}>
                 <Box style={{ minHeight: '500px' }} flex align='start' justify='center'>
-                    <TextInput value={searchQueary} onChange={onChange} />
-                    <SongList />
+                  <RadioButtonGroup
+                    name="searchByX"
+                    options={["Artist", "Song"]}
+                    value={clientState.searchCategory}
+                    onChange={handleSearchCategory}
+                  />
+                    <TextInput value={clientState.searchQueary} onChange={onChange} />
+                    {clientState.searchCategory === 'Song' ? <SongList /> : <ArtistList/> }
                 </Box>
             </Box>
         </AppWrapper >
     )
-} 
+}
